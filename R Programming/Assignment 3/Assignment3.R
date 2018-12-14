44bc5ae8b91795234581b046819aa3c44d312bf5
julia-teng <julia.teng83@gmail.com>
2018-12-14 14:43:34 -0500

    part 2 of the assignment

--- R Programming/Assignment 3/Assignment3.R
+++ R Programming/Assignment 3/Assignment3.R
@@ -5,6 +5,39 @@ setwd("C:/users/julia.teng/Desktop/Julia/Training/Data Science Specialization/da
 ## Part 1 - Plot the 30 day mortality rates for heart attack
 ## read file
 outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-
 outcome[, 11] <- as.numeric(outcome[, 11])
-hist(outcome[, 11])
+hist(outcome[, 11],main = "Death Rates from Heart Attack (30 days)",xlab="Death Rates (30 days)")
+
+## Part 2 - Finding the best hospital in state
+best <- function(state,outcome){
+  
+  ## Read outcome data
+  outtable <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  data <- as.data.frame(cbind(outtable[,2], # Hospital
+                              outtable[,7], # State
+                              outtable[,11], # Heart Attack
+                              outtable[,17], # Heart Failure
+                              outtable[,23]), # Pneumonia
+                              stringsAsFactors = FALSE
+                              )
+  colnames(data)<-c("Hospital","State","heart attack","heart failure","pneumonia")
+  
+  ## Check that state and outcome are valid
+  if(state %in% data$State & outcome %in%c("heart attack","heart failure","pneumonia")){
+    # check state return id
+    isState<-which(data[,"State"]==state)
+    StateData<-data[isState,]
+    # get outcome value
+    StateOutcome<-as.numeric(StateData[,eval(outcome)])
+    min_val<-min(StateOutcome,na.rm=TRUE)
+    result<-StateData[,"Hospital"][which(StateOutcome==min_val)]
+    rank <- result[order(result)]
+  }else if (!state %in% data$State){
+    stop('invalid state')
+  }else if (!outcome %in% c("heart attack","heart failure","pneumonia")){
+    stop('invalid outcome')
+  }
+  ## retunr hospital name in that state with lowest 30 day death rate
+  rank
+}
+    
